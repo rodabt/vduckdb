ccbdcd6861ecaa1fd7aa8736e20b4fb4ebbea1b1
Rodrigo Abt <rodrigo.abt@gmail.com>
2023-05-20 17:34:40 -0400

    Updated types

--- duckdb/wrapper.v
+++ duckdb/wrapper.v
@@ -23,33 +23,33 @@ pub enum State {
 }
 
 fn C.duckdb_open(path &char, database &Database) State
-fn C.duckdb_connect(database voidptr, connection &Connection) State
-fn C.duckdb_query(connection voidptr, query &char, results &Results) State
-fn C.duckdb_disconnect(connection voidptr)
-fn C.duckdb_close(database voidptr)
+fn C.duckdb_connect(database &Database, connection &Connection) State
+fn C.duckdb_query(connection &Connection, query &char, results &Results) State
+fn C.duckdb_disconnect(connection &Connection)
+fn C.duckdb_close(database &Database)
 fn C.duckdb_library_version() &char
-fn C.duckdb_destroy_result(results voidptr)
-fn C.duckdb_row_count(results voidptr) u64
-fn C.duckdb_column_count(results voidptr) u64
+fn C.duckdb_destroy_result(results &Results)
+fn C.duckdb_row_count(results &Results) u64
+fn C.duckdb_column_count(results &Results) u64
 
 
 pub fn open(path &char, database &Database) State {
 	return C.duckdb_open(path, database)
 }
 
-pub fn connect(database voidptr, connection &Connection) State {
+pub fn connect(database &Database, connection &Connection) State {
 	return C.duckdb_connect(database, connection)
 }
 
-pub fn query(connection voidptr, query &char, results &Results) State {
+pub fn query(connection &Connection, query &char, results &Results) State {
 	return C.duckdb_query(connection, query, results)
 }
 
-pub fn disconnect(connection voidptr) {
+pub fn disconnect(connection &Connection) {
 	C.duckdb_disconnect(connection)
 }
 
-pub fn close(database voidptr) {
+pub fn close(database &Database) {
 	C.duckdb_close(database)
 }
 
@@ -57,14 +57,14 @@ pub fn library_version() &char {
 	return C.duckdb_library_version()
 }
 
-pub fn destroy_result(results voidptr) {
+pub fn destroy_result(results &Results) {
 	C.duckdb_destroy_result(results)
 }
 
-pub fn row_count(results voidptr) u64 {
+pub fn row_count(results &Results) u64 {
 	return C.duckdb_row_count(results)
 }
 
-pub fn column_count(results voidptr) u64 {
+pub fn column_count(results &Results) u64 {
 	return C.duckdb_column_count(results)
 }
