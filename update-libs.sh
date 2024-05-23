e6d41780bf5c855372bb7cb98a8e36d845fb56ab
Rodrigo Abt <rodrigo.abt@gmail.com>
2024-05-23 12:33:45 -0400

    Updated lib management script

--- update-libs.sh
+++ update-libs.sh
@@ -6,6 +6,7 @@ LIB_MAC="https://github.com/duckdb/duckdb/releases/download/${LATEST_TAG}/libduc
 LIB_WINDOWS="https://github.com/duckdb/duckdb/releases/download/${LATEST_TAG}/libduckdb-windows-amd64.zip"
 
 echo "Updating..."
+rm ./src/thirdparty/*
 wget $LIB_LINUX -P ./src/thirdparty/
 wget $LIB_MAC -P ./src/thirdparty/
 wget $LIB_WINDOWS -P ./src/thirdparty/
@@ -15,4 +16,5 @@ unzip -j -o ./src/thirdparty/libduckdb-linux-amd64.zip -d "./src/thirdparty/" li
 unzip -j -o ./src/thirdparty/libduckdb-osx-universal.zip -d "./src/thirdparty/" libduckdb.dylib
 unzip -j -o ./src/thirdparty/libduckdb-windows-amd64.zip -d "./src/thirdparty/" duckdb.dll 
 mv ./src/thirdparty/duckdb.dll ./src/thirdparty/libduckdb.dll
+rm ./src/thirdparty/*.zip
 
