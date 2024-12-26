b378922d885101411741c068d8dc4f8a5305ff9b
Rodrigo Abt <rodrigo.abt@gmail.com>
2024-12-26 16:06:52 -0300

    Minor adjust

--- src/globals.v
+++ src/globals.v
@@ -4,11 +4,16 @@ import dl
 import time
 import os
 
-const library_file_path = if os.getenv('LIBDUCKDB_DIR') == '' {
-	dl.get_libname('thirdparty/libduckdb')
-} else {
-	dl.get_libname('${os.getenv('LIBDUCKDB_DIR')}/libduckdb')
+fn get_libname() string {
+	libname:= if os.getenv('LIBDUCKDB_DIR') == '' {
+		dl.get_libname('thirdparty/libduckdb')
+	} else {
+		dl.get_libname('${os.getenv('LIBDUCKDB_DIR')}/libduckdb')
+	}
+	return libname
 }
+
+const library_file_path = get_libname()
 const start_date = time.Time{
 	year:  1970
 	month: 1
